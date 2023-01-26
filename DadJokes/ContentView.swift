fbcc579246c7c67fbe613f9ea55a9f7c8a6b07a8
Tymeee <88166167+Tymeee@users.noreply.github.com>
2023-01-26 16:21:09 +0700

    Initial Commit

---
+++ DadJokes/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DadJokes
+//
+//  Created by E2318556 on 26/1/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by E2318556 on 26/1/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
